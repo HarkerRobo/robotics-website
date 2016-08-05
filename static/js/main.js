7d3ca10ea5af057d1ad773ce75dcd0ba8a91ed51
DJ Melisso <djm@deltavbio.com>
2016-08-05 14:34:59 -0700

    made logs tile, added .gitignore
    -gitignored /node-modules/
    -gitignored media
    -got rid of blue border focus
    -finished learn-more-button
    -made nice <a>’s
    -changed filters
    -made logs tile
    -added konami code

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,31 @@
+function konami() {
+  window.open('http://youtube.com');
+}
+
+
 $(function(){
+
+  // konami code
+  var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+
+  $(document).keydown(function(e) {
+
+    kkeys.push( e.keyCode );
+
+    if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+
+
+      // do something awesome
+
+      window.open('https://www.youtube.com/watch?v=ApcFBZVbAPA');
+      kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+
+    }
+
+  });
+
+
+
   $(':checkbox').checkboxpicker();
   $(".landingtext").fadeIn(1500);
 
