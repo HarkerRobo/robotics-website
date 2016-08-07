8fbcb980bb9adff346a7b1fb7f1f21243bd5843b
DJ Melisso <djm@deltavbio.com>
2016-08-06 22:01:12 -0700

    added robots.txt, description, made snapping scroll

--- static/js/index-js.js
+++ static/js/index-js.js
@@ -1,8 +1,24 @@
 'use strict'
 
 var tiles = []
+var $root = $('html, body')
 
 $(function(){
+
+  // scroll to on nav click
+  $('nav a').click(function() {
+    var href = $.attr(this, 'href')
+    var gotoname = $.attr(this, 'gotoname')
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, function () {
+      window.location.hash = gotoname
+    })
+    return false
+  })
+
+
+  // updates hash and navbar
   tiles = $('header, section')
 
   hashUpdate()
@@ -10,6 +26,37 @@ $(function(){
 
   $(document).scroll(hashUpdate)
   $(document).scroll(navUpdate)
+
+
+  // snaps on stop scrolling
+  $(window).scroll(function() {
+    clearTimeout($.data(this, 'scrollTimer'));
+
+    $.data(this, 'scrollTimer', setTimeout(function() {
+
+      console.log("Haven't scrolled in 250ms!");
+
+      var scrollPosition = window.scrollY
+      var amt = Math.abs($(tiles[0]).offset().top - scrollPosition)
+      var lastPast = tiles[0]
+      tiles.each(function(index, value){
+        if(Math.abs($(lastPast).offset().top - scrollPosition) > Math.abs($(value).offset().top) - scrollPosition)
+        {
+          lastPast = value
+        }
+      })
+
+      var id = "#" + $.attr(lastPast, 'id')
+      var hashname = $.attr(lastPast, 'hashname')
+      $root.animate({
+        scrollTop: $(id).offset().top
+      }, 500, function () {
+        window.location.hash = hashname
+      })
+
+
+    }, 500));
+  });
 })
 
 function navUpdate() {
