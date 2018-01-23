02bc56dd919282f98639fc5f53dc15f4b8a7f665
DJ Melisso <djm@deltavbio.com>
2018-01-22 20:48:00 -0800

    modularized auth middleware

---
+++ helpers/auth.js
@@ -0,0 +1,33 @@
+const sessionAuth = (req, res, next) => {
+  req.auth = req.session.auth
+  if (!req.auth) req.auth = { loggedin: false }
+  res.locals.auth = req.auth
+  next()
+}
+
+const verifyLogin = (req, res, next) => {
+  sessionAuth(req, res, () => {
+    if (req.auth.loggedin) {
+      console.log('Auth: ' + req.auth.info.name + ' (' + req.auth.info.email + ')')
+      console.log('Auth level: ' + req.auth.level)
+      next()
+    } else {
+      res.render('pages/member/login')
+    }
+  })
+}
+
+const verifyRank = rank => {
+  return (req, res, next) => {
+    verifyLogin(req, res, () => {
+      if (req.auth.level > rank) next()
+      else res.render('pages/member/error', { statusCode: 401, error: "You must have higher clearance to reach this page."})
+    })
+  }
+}
+
+module.exports = {
+  sessionAuth: sessionAuth,
+  verifyLogin: verifyLogin,
+  verifyRank: verifyRank,
+}
