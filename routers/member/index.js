eaa8c941637931f8b85b8f8806069ffe95053f1b
DJ Melisso <djm@deltavbio.com>
2018-01-28 15:19:25 -0800

    small fix

--- routers/member/index.js
+++ routers/member/index.js
@@ -77,7 +77,7 @@ router.post('/token', function (req, res) {
         result.on('data', (d) => { data += d }).on('end', (d) => {
           data = JSON.parse(data)
           if (result.statusCode === 200) {
-            if (config.GoogleClientID.includes(data.aud)) {
+            if (config.GoogleClientIDs.includes(data.aud)) {
               req.session.auth = { loggedin: true, token: token, info: data }
               console.log(data.name + ' (' + data.email + ')')
 
