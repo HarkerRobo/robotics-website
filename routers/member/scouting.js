b849ddce9496de88bed62d8980fb06d8f5c147c1
Ryan Adolf <ryanadolf123@gmail.com>
2018-03-28 16:22:26 -0700

    Parse json strings

--- routers/member/scouting.js
+++ routers/member/scouting.js
@@ -573,6 +573,8 @@ router.get('/data/tsv', (req, res) => {
         const data = round[color][team]
         if (!data.data) continue
         const d = data.data
+        if (typeof d['auton-actions'] === 'string') d['auton-actions'] = JSON.parse(d['auton-actions'])
+        if (typeof d['teleop-actions'] === 'string') d['teleop-actions'] = JSON.parse(d['teleop-actions'])
         res.write(`\n${round.number}\t${team}\t${color}\t${data.number}\t${data.scout}\t${d.start_position}\t${d.crossed_line}`
                 + `\t${d.end_platform}\t${d.lift}`
                 + `\t${d['auton-actions'].filter(a=>a.action=='0_0_0'||a.action=='0_0_2').length}`
