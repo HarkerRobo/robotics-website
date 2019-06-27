356e11417fc1835ae39fc53b08f7caaed7b0f565
Chirag Kaushik <22chiragk@students.harker.org>
2019-06-26 21:58:53 -0700

    Changed langind text, various adjustments

--- static/new/js/index.js
+++ static/new/js/index.js
@@ -35,3 +35,51 @@ setInterval(function() {
         //document.getElementById("member-link").style.visibility = "visible";
     }
 }, 1000);
+
+const landingText = document.getElementById("landing-banner-text");
+const texts = ["Engineers", "Inventors", "Problem Solvers", "Family", "Harker Robotics"];
+let textIndex = 0;
+let text = texts[textIndex];
+let index = 0;
+let typing = false;
+
+function addLetter() {
+    index += 1;
+    landingText.innerHTML = "We are " + text.substring(0, index) + (typing ? "|" : "");
+    if(index == text.length) {
+        setTimeout(removeLetter, 2000);
+    } else {
+        setTimeout(addLetter, 150);
+    }
+}
+
+function removeLetter() {
+    index -= 1;
+    landingText.innerHTML = "We are " + text.substring(0, index) + (typing ? "|" : "");
+    if(!index) {
+        //landingText.innerHTML = "We are" + (typing ? "|" : "");
+    }
+    if(index + 1) {
+        setTimeout(removeLetter, 150);
+    } else {
+        textIndex++;
+        text = texts[textIndex % texts.length];
+        setTimeout(addLetter, 150);
+    }
+}
+
+setInterval(function() {
+    typing = typing == false;
+    landingText.style.marginLeft = typing ? "1.94vw" : "0";
+    if(typing && !landingText.innerHTML.endsWith("|")) {
+        landingText.innerHTML += "|";
+    } else if(!typing && landingText.innerHTML.endsWith("|")) {
+        landingText.innerHTML = landingText.innerHTML.slice(0, -1);
+    }
+}, 800)
+
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        addLetter();
+    }, 1100);
+});
