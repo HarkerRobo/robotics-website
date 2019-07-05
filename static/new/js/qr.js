1c57f74588db973c563dfda915c057fd4abb5e53
Chirag Kaushik <22chiragk@students.harker.org>
2019-07-04 18:36:36 -0700

    I don't know how to code

--- static/new/js/qr.js
+++ static/new/js/qr.js
@@ -1,6 +1,6 @@
 function resetQR() {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "../attendance/qrcode", true);
+    xhr.open("GET", "./attendance/qrcode", true);
     xhr.onload = () => {
         const data = JSON.parse(xhr.responseText);
         QRCode.toCanvas(document.getElementById("qr-canvas"), data.data, {
