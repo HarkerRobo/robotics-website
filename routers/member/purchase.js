df408d14a9b9a0487ce570e4b1dfd680ff3c721c
CMEONE <contact@kabirwearsato.ga>
2022-09-21 10:41:12 -0700

    Fix string escape

--- routers/member/purchase.js
+++ routers/member/purchase.js
@@ -112,7 +112,7 @@ router.get(
                     email: purchase.submitted_by,
                 })
             ) || {name: "Harker Robotics"}).name;
-            const email_link = `mailto:Eric.Nelson@harker.org?subject=Receipt for Reimbursement (PR #${purchase.purchase_id})&body=Hi Dr. Nelson,%0A%0AHere's the receipt for reimbursement for PR #${purchase.purchase_id} (https://robotics.harker.org/member/purchase/view/${purchase.purchase_id}).%0A%0AThanks,%0A${req.auth.info.name}`;
+            const email_link = `mailto:Eric.Nelson@harker.org?subject=Receipt for Reimbursement (PR #${purchase.purchase_id})&body=Hi Dr. Nelson,%0A%0AHere\\'s the receipt for reimbursement for PR #${purchase.purchase_id} (https://robotics.harker.org/member/purchase/view/${purchase.purchase_id}).%0A%0AThanks,%0A${req.auth.info.name}`;
             res.render("pages/member/purchase/view", {
                 purchase: purchase,
                 creation: purchase._id.getTimestamp().toDateString(),
